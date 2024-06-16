c68fb60b540eb165063f5978334c66a60b7787bc
plabacki <plabacki97@gmail.com>
2024-06-16 20:36:15 +0200

    Added validations to expense model

--- Models/Expense.cs
+++ Models/Expense.cs
@@ -6,7 +6,12 @@ namespace BudgetMvcApp.Models;
 public class Expense
 {
     public int Id {get;set;}
+    [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+    [StringLength(30, MinimumLength = 2)]
+    [Required]
     public string? Name {get;set;}
+    [Range(1,500)]
+    [DataType(DataType.Currency)]
     public decimal Amount {get;set;}
     [DataType(DataType.Date)]
     public DateTime Date {get;set;}
