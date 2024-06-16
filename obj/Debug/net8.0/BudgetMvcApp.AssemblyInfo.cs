72c58b00cfc979692ec82a8a4434ee06e764c6dc
plabacki <plabacki97@gmail.com>
2024-06-16 15:35:44 +0200

    Added view for expense index

--- obj/Debug/net8.0/BudgetMvcApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/BudgetMvcApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BudgetMvcApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf17098f18107f42fe500d9728ab45cba6397fa0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+578cd8d8d738671526692dd520fcce33aae6f569")]
 [assembly: System.Reflection.AssemblyProductAttribute("BudgetMvcApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BudgetMvcApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
