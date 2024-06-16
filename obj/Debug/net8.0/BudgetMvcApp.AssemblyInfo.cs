bf8c7d3e02795150e425b4a52b00835a6e717f98
plabacki <plabacki97@gmail.com>
2024-06-16 16:14:08 +0200

    Created view for transaction index

--- obj/Debug/net8.0/BudgetMvcApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/BudgetMvcApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BudgetMvcApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e548fd5bbeefa1fb8ff9c4d2619e9b2861db5a7c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a2ae543f9e238a52c139a945f940928cdf73d40")]
 [assembly: System.Reflection.AssemblyProductAttribute("BudgetMvcApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BudgetMvcApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
