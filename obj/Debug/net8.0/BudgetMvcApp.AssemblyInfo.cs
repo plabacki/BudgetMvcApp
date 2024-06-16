93f0ba1819b6a5d144bcbacdaeec009d76ce76a1
plabacki <plabacki97@gmail.com>
2024-06-16 20:09:39 +0200

    Crated edit view for transaction

--- obj/Debug/net8.0/BudgetMvcApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/BudgetMvcApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BudgetMvcApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94e01ca21dba326e8b84d6c14e2533e786e18a86")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cedc3ad8c87e17f65a0a6c190062df518927d921")]
 [assembly: System.Reflection.AssemblyProductAttribute("BudgetMvcApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BudgetMvcApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
